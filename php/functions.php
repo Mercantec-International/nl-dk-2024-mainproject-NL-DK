e370698c558ba5c5aeb09dacebff6a27436e33e8
Wiggedbucket <lucasvanhest2005@gmail.com>
2024-11-12 08:56:00 +0100

    Header default function

---
+++ php/functions.php
@@ -0,0 +1,20 @@
+<?php
+
+function headerHTML($title = "Default")
+{
+    ?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title><?php echo $title; ?> | Project Apollo</title>
+    </head>
+    <body>
+        
+    </body>
+    </html>
+    <?php
+}
+
+?>
