23bc39dafbcdbae869f3618dc30c42366bac7ff6
TGSXVII <madsduly@gmail.com>
2024-12-11 13:50:51 +0100

    stuff and stuff

--- app/rc_controller/lib/widgets/custom_list_item.dart
+++ app/rc_controller/lib/widgets/custom_list_item.dart
@@ -6,8 +6,13 @@ import 'package:flutter/material.dart';
 import '../classes/helper/api.dart';
 
 class CustomListItem extends StatelessWidget {
-  CustomListItem({super.key, required this.id, required this.lastEmergency, 
-      required this.createdAt, required this.updateFunc, required this.index}) {}
+  CustomListItem(
+      {super.key,
+      required this.id,
+      required this.lastEmergency,
+      required this.createdAt,
+      required this.updateFunc,
+      required this.index}) {}
   final String id;
   final DateTime lastEmergency, createdAt;
   final int index;
@@ -19,81 +24,107 @@ class CustomListItem extends StatelessWidget {
     return Column(
       children: [
         GestureDetector(
-            onTap: () async {
-              // TODO: GO TO CONTROLLER
-              //await Navigator.push(context, MaterialPageRoute(builder: (context) => ControllerPage()));
-            },
-            child: SizedBox(
-              width: MediaQuery.of(context).size.width,
-              child: Container(
-                color: Colors.blue,
-                padding: const EdgeInsets.all(10),
-                child: DefaultTextStyle(
-                  style: TextStyle(
-                    fontSize: General.isPhone ? null : 25,
-                    color: Colors.white,
-                  ),
-                  child: Row(
-                    children: [
-                      SizedBox(
-                        width: MediaQuery.of(context).size.width - 70,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                //Title
-                                const Text("Id:"),
-                                //Content
-                                SizedBox(
-                                  width: MediaQuery.of(context).size.width - 55,
-                                  child: Text(id,
-                                    maxLines: 2,
-                                    overflow: TextOverflow.ellipsis,
-                                    style: TextStyle(fontSize: General.isPhone ? 20 : 35),
-                                  ),
-                                )
-                              ],
-                            ),
-                            const SizedBox(height: 10),
-                            Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const Text("Last emergency:"),
-                                Text(
-                                  lastEmergency.toString(), maxLines: 1,
+          onTap: () async {
+            // TODO: GO TO CONTROLLER
+            await Navigator.push(context,
+                MaterialPageRoute(builder: (context) => ControllerPage()));
+          },
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: Container(
+              color: Colors.blue,
+              padding: const EdgeInsets.all(10),
+              child: DefaultTextStyle(
+                style: TextStyle(
+                  fontSize: General.isPhone ? null : 25,
+                  color: Colors.white,
+                ),
+                child: Row(
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width - 70,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              //Title
+                              const Text("Id:"),
+                              //Content
+                              SizedBox(
+                                width: MediaQuery.of(context).size.width - 55,
+                                child: Text(
+                                  id,
+                                  maxLines: 2,
                                   overflow: TextOverflow.ellipsis,
-                                  style: TextStyle(fontSize: General.isPhone ? 20 : 35),
+                                  style: TextStyle(
+                                      fontSize: General.isPhone ? 20 : 35),
                                 ),
-                              ],
-                            ),
-                            Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const SizedBox(height: 10),
-                                const Text('Created at:'),
-                                Text(
-                                  createdAt.toString(), maxLines: 1,
-                                  overflow: TextOverflow.ellipsis,
-                                  style: TextStyle(fontSize: General.isPhone ? 20 : 35),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
+                              )
+                            ],
+                          ),
+                          const SizedBox(height: 10),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text("Last emergency:"),
+                              Text(
+                                lastEmergency.toString(),
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                    fontSize: General.isPhone ? 20 : 35),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const SizedBox(height: 10),
+                              const Text('Created at:'),
+                              Text(
+                                createdAt.toString(),
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                    fontSize: General.isPhone ? 20 : 35),
+                              ),
+                            ],
+                          ),
+                        ],
                       ),
-                      const Spacer(),
-                    ],
-                  ),
+                    ),
+                    const Spacer(),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ControllerPage(title: id),
+                          ),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.black,
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20, vertical: 10),
+                      ),
+                      child: const Text(
+                        'Control Car',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
           ),
+        ),
       ],
     );
   }
